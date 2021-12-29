25b9505b011ccdda7d4368baf0d21e010c351a45
hyean.qin <hyean.qin@gmail.com>
2021-12-29 23:22:30 +0800

    use the function of sys.argv,which user can input argv from command

--- guess.py
+++ guess.py
@@ -1,8 +1,11 @@
 import random
+import sys	# 系统模块
 
 lucky = random.randint(0,101)
 low, high = 0, 100
-guess_limit, guess_count = 3, 0
+guess_limit, guess_count = int(sys.argv[1]), 0
+# sys.argv[]  就是取从命令行传入的参数， 命令行输入 python guess.py  3----相当于传了guess.py 和 3这两个参数
+# print(sys.argv)
 # print(lucky)
 guess = int(input(f'我想了一个幸运数字，范围是[{low},{high}],请你来猜一猜呀~\n温馨提示：只有{guess_limit}次尝试机会哦~请好好考虑后再开始猜数哈~\n'))
 while (guess_limit > guess_count):
@@ -10,10 +13,10 @@ while (guess_limit > guess_count):
 	if lucky == guess:
 		print("哇哦~你猜中了耶！恭喜你！")
 		break
-	elif lucky > guess and guess_count < 3:
+	elif lucky > guess and guess_count < guess_limit:
 		low = guess
 		guess = int(input(f"不好意思，幸运数字在({low},{high}]\n请继续--->"))
-	elif lucky < guess and guess_count <3:
+	elif lucky < guess and guess_count < guess_limit:
 		high = guess
 		guess = int(input(f"不好意思，幸运数字在[{low},{high})\n请继续--->"))
 	else:
